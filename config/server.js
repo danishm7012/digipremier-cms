eb7c8dcb5b575c3c91c5d30f37bb355a040debb8
Danish Mehmood <danishm7012@gmail.com>
2021-07-13 14:10:45 +0500

    deployment setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b2384f3e08fd60fd6e568ee61d77fc39'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f07cc8e9-bf99-4a2d-94eb-f16248dfcc1a'
+};
